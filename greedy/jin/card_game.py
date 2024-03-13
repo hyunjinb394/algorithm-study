f9b0f66ba2baf2941e47d89358249ae0e47e5fa5
hyunjinb <hyunjinb394@gmail.com>
2024-03-14 03:27:09 +0900

    현진 1주차 greedy 문제풀이 완료

---
+++ greedy/jin/card_game.py
@@ -0,0 +1,32 @@
+# 1. 문제 
+# N * M 형태의 숫자 카드 중 각 행마다 가장 작은 수를 찾는다.
+# 그 중에서 가장 큰 수를 찾는다.
+
+# 2. 고민한 방식
+n, m = map(int, input().split())
+
+row_card = []
+row_min = []
+
+
+for i in range(n):
+        row_card.append(list(map(int,input().split())))
+        row_min.append(min(row_card[i]))
+
+answer = max(row_min)
+
+print(answer);
+
+
+# 3. 더 효율적인 방식
+n, m = map(int, input().split())
+
+result = 0
+
+for i in range(n):    
+    card = list(map(int, input().split()))    
+    min_value = min(card)    
+    result = max(min_value, result)    
+
+print(result)
+
