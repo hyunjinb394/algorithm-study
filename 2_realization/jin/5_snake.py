ecf6747d588daec0238d97e2dc2841a1aced9054
hyunjinb <hyunjinb394@gmail.com>
2024-03-24 01:51:59 +0900

    Comment : 4_lock_key 주석 추가

--- 2_realization/jin/5_snake.py
+++ 2_realization/jin/5_snake.py
@@ -30,13 +30,14 @@ def start():
     time = 1  # 시간
     y, x = 0, 0  # 초기 뱀 위치
     visited = deque([[y, x]])  # 방문 위치
-    arr[y][x] = 2
+    arr[y][x] = 2  # 게임보드, 뱀의 위치는 2
     while True:
         y, x = y + dy[direction], x + dx[direction]
+        # 게임 보드 내에 있고 자기 자신의 몸통에 부딪히지 않는 경우
         if 0 <= y < N and 0 <= x < N and arr[y][x] != 2:
             if not arr[y][x] == 1:  # 사과가 없는 경우
-                temp_y, temp_x = visited.popleft()
-                arr[temp_y][temp_x] = 0  # 꼬리 제거
+                temp_y, temp_x = visited.popleft() # 꼬리 제거
+                arr[temp_y][temp_x] = 0  
             arr[y][x] = 2
             visited.append([y, x])
             if time in times.keys():
