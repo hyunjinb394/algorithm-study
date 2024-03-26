252c85f06932b5b792169b43d72337756fd479a2
hiddenbebb <cbj4133@naver.com>
2024-03-26 23:52:27 +0900

    벽뿌시기 제외 완료

--- 3_DFS_BFS/bong/else/b_stack.py
+++ 3_DFS_BFS/bong/else/b_stack.py
@@ -1,29 +1,37 @@
 #https://www.acmicpc.net/problem/1874
-#
+# 문제 설명
+# 1부터 n까지의 수가 존재, 이 수들을 순서대로 나열해야함.
+# 나열 하기이전에 stack[]이라는 공간에 넣었다 뽑음.
+# 뽑아내야 하는 순서는 input에 주어지는 임의의 수열
+
+
+#코드 순서
+#1. stack에 1부터 넣음. 근데 넣을 때 만일 squence에 들어있는 수가 있다면 그수는 제거
+#2. 만일 가장 stack 위에있는 수가 지금 빼야 할 숫자보다 클시 'no' 출력(왜냐하면 이건 어짜피 실패하는 코드가 되기 때문) 
+def sol(sequence):
+    operations = []   # 답을 저장하는 공간
+    stack = []        # 스택 공간
+    current = 1       # 현재 넣어야 할 수 변수
+
+    for num in sequence:
+        # 현재 넣어야 할 수(num)보다 큰 수가 스택에 있는 경우
+        while current <= num:
+            stack.append(current)
+            operations.append('+')
+            current += 1
+        
+        # 스택의 맨 위에 있는 수가 현재 수열의 수와 같은 경우
+        if stack[-1] == num:
+            stack.pop()
+            operations.append('-')
+        else:
+            # 스택의 맨 위에 있는 수가 현재 수열의 수와 다른 경우
+            return 'NO'
+    return operations
 
 n = 8
-sequence = [4,3,6,8,7,5,2,1]
-operations = []
+sequence1 = [4,3,6,8,7,5,2,1]
+n = 5
+sequence2 = [1,2,5,3,4]
 
-stack = []        # 스택 초기화
-current = 1       # 현재 넣어야 할 수
-
-for num in sequence:
-    # 현재 넣어야 할 수(num)보다 큰 수가 스택에 있는 경우
-    while current <= num:
-        stack.append(current)
-        operations.append('+')
-        current += 1
-    
-    # 스택의 맨 위에 있는 수가 현재 수열의 수와 같은 경우
-    if stack[-1] == num:
-        stack.pop()
-        operations.append('-')
-    else:
-        # 스택의 맨 위에 있는 수가 현재 수열의 수와 다른 경우
-        print('NO')
-        exit(0)
-
-# 연산 기록 출력
-for op in operations:
-    print(op)
+print(sol(sequence1))
---
+++ 3_DFS_BFS/bong/else/hj_shortest_game.py
@@ -0,0 +1,26 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/1844
+
+from collections import deque
+
+def sol(map):
+    n = len(map)                            #지도의 가로 길이
+    m = len(map[0])                         #지도의 세로 길이
+    direc = [(-1,0),(1,0),(0,-1),(0,1)]     #방향 설정    
+
+    que = deque([(0,0,1)])                  #위치 + 지나간 거리
+    
+    while que:
+        x,y,move = que.popleft()
+        if x == n - 1 and y == m - 1:  # 상대 팀 진영 도착 시 거리 반환
+            return move
+
+        for dx, dy in direc:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < n and 0 <= ny < m and map[nx][ny] == 1:
+                que.append((nx, ny, move + 1))
+                map[nx][ny] = 0  # 방문한 곳은 0으로 표시하여 다시 방문하지 않도록 함
+
+    return -1  # 상대 팀 진영에 도착할 수 없는 경우
+
+a =[[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]
+print(sol(a))
