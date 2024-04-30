53e7f39647dc3ae485b6545d3293021d727a5855
bongjooncha <cbj4133@naver.com>
2024-04-30 21:40:33 +0900

    이동

--- 5_binary_search/bong/else/blackfriday.py
+++ 5_binary_search/bong/else/blackfriday.py
@@ -1,2 +1,32 @@
 #https://www.acmicpc.net/problem/18114
-# 
+# 10^8무게, 5000개의 물건
+
+# n ,c = map(int,input().split())
+# w = list(map(int,input().split()))
+n, c = 3,13
+w= [3,7,8] 
+
+def check(weights, target_weight, num_items):
+    # 물건들의 무게를 오름차순으로 정렬
+    weights.sort()
+
+    # 이진 탐색을 사용하여 가능한 조합을 찾음
+    left = 0
+    right = num_items - 1
+
+    while left <= right:
+        mid = left + (right - left) // 2
+        # 선택한 물건들의 무게의 합이 목표 무게와 같은지 확인
+        if weights[mid] == target_weight:
+            return True
+        elif weights[mid] < target_weight:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return False
+for i in range(n):
+    if check(w,c,n):
+        print(1)
+        exit()
+print(0)
--- 5_binary_search/bong/else/game.py
+++ 5_binary_search/bong/else/game.py
@@ -1,3 +1,17 @@
 # https://www.acmicpc.net/problem/1072
 # 게임 횟수 10^9회 이하.
 # 제한 시간 2초이므로 N회 불가능. 
+
+
+def additional_games(X, Y):
+    Z = (Y * 100) // X
+    if Z == 100:
+        return -1
+    return (X*(Z+1)-100*Y)/(100-(Z+1))
+    
+
+# 입력 받기
+X, Y = map(int, input().split())
+
+# 결과 출력
+print(additional_games(X, Y))
