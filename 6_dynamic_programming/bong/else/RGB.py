59ee9ac21a917caca0b13dccb27167abe71ba358
hiddenbebb <cbj4133@naver.com>
2024-05-13 23:16:39 +0900

    2문제 남음

--- 6_dynamic_programming/bong/else/RGB.py
+++ 6_dynamic_programming/bong/else/RGB.py
@@ -1,2 +1,19 @@
+# https://www.acmicpc.net/problem/1149
 # 집수 N은 2이상 1,000이하
 # 집 칠하는 비용 1,000이하
+
+n = int(input())
+cost = [list(map(int, input().split())) for _ in range(n)]
+
+# C는 cost, N은 집수(N회 반복. 좀더 정확히는 3N이 맞을듯)
+def sol(C):
+    ans=[0,0,0]
+    re = [0,0,0]
+    for i in C:             #i는 집의 페인트 cost
+        ans[0] = i[0] + min(re[1],re[2])
+        ans[1] = i[1] + min(re[0],re[2])
+        ans[2] = i[2] + min(re[0],re[1])
+        re = ans[:]         #깊은 복사 해야함
+    return min(ans)
+            
+print(sol(cost))
---
+++ 6_dynamic_programming/bong/else/onefive.py
@@ -0,0 +1,29 @@
+# https://www.acmicpc.net/problem/15990
+# N은 10^5이하
+
+# 뒤에 숫자가 중요. 뒤가 1을 더하는 경우 -1한 수의 2,3으로 끝나는 수, 2더하는경우 -2한수의 1,3으로 끝나는수, 3더하는경우도 마찬가지.
+# index error가 뜨는데 왜 뜨는지 모르겠어요...
+n = int(input())
+inp = [int(input()) for _ in range(n)]
+large = inp[-1]
+
+def sol(L):   #L은 가장 큰수
+    ans = [[1,0,0],[0,1,0],[1,1,1]]
+
+    if L <= 3: return ans
+
+    for i in range(3,L):       
+        # k = [0,0,0]                               # 현진님 주석 처리한 부분 k= [0,0,0]으로 초기화 하지 않으면 이상하게 나와요
+        # k[0] = ans[i-1][1] + ans[i-1][2]          # 이유를 모르겠어요 ㅠㅠㅠ 발표할때 알려줘용....ㅎㅎ
+        # k[1] = ans[i-2][0] + ans[i-2][2]
+        # k[2] = ans[i-3][0] + ans[i-3][1]
+        # ans.append(k)
+        ans.append([ans[i-1][1] + ans[i-1][2], ans[i-2][0] + ans[i-2][2], ans[i-3][0] + ans[i-3][1]])
+
+    return ans
+
+ans = sol(large)
+
+
+for i in inp:
+    print(sum(ans[i-1])% 1_000_000_009)
